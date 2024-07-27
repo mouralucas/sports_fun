02e809666919c8e01a29688faabb7e8099e0151f
Lucas Moura <lucaspenha471@gmail.com>
2024-07-27 10:05:39 -0300

    Add entity league

---
+++ src/main/java/com/rolf/sports_fun/entities/League.java
@@ -0,0 +1,30 @@
+package com.rolf.sports_fun.entities;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+
+@Entity
+public class League extends BaseEntity {
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
